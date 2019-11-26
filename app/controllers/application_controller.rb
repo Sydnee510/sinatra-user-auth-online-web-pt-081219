2a7b35babfeccfb825994fee0461c9bee58c1930
sydnee510 <sbsydnee161416@gmail.com>
2019-11-25 21:26:52 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,12 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    puts params
     @user.save
     session[:user_id] = @user.id
-
+    
     redirect '/users/home'
+    
   end
 
   get '/sessions/login' do
@@ -32,6 +34,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
+    puts params
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
